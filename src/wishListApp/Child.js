790e612b7a395d1dec1aaccb8204102a8d571786
Gregory.Johnson <3531op@gmail.com>
2021-03-04 13:50:52 -0500

    final submit

---
+++ src/wishListApp/Child.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Button from "./common/Button";
+
+import "./Child1-ul.css";
+
+export class Child extends Component {
+  state = {
+    toggleInput: "",
+  };
+
+  handleToggleOnChange = (event) => {
+    this.setState({
+      toggleInput: event.target.value,
+    });
+  };
+
+  handleToggleButton = (id, wishItem) => {
+    this.setState({
+      toggleInput: wishItem,
+    });
+    this.props.handleEditToggle(id);
+    this.props.updateWish(id, this.state.toggleInput);
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.props.wishList.map((item) => {
+          let strikeThroughClass = `${
+            item.isDone ? "strike-through-isDone" : ""
+          }`;
+
+          return (
+            <React.Fragment key={item.id}>
+              {/* <li style={{ textDecoration: item.isDone ? "line-through" : "" }}> */}
+              {/* <li className={`${item.isDone ? "strike-through-isDone" : ""}`}> */}
+              {/* <li style={{ textDecoration: item.isDone && "line-through" }}> */}
+              {/* <li className={strikeThroughClass}>{item.wish}</li>{" "} */}
+
+              {item.isEditToggle ? (
+                <input
+                  value={this.state.toggleInput}
+                  style={{ marginRight: 10 }}
+                  onChange={this.handleToggleOnChange}
+                  name="toggleInput"
+                />
+              ) : (
+                <li className={strikeThroughClass}>{item.wish}</li>
+              )}
+
+              <Button
+                propsButtonToggle={item.isButtonToggle}
+                propsClassName={"btn btn-success button-style"}
+                propsName={item.isEditToggle ? "Submit" : "Edit"}
+                propsOnClick={() => this.handleToggleButton(item.id, item.wish)}
+                //propsOnClick={() => this.props.handleEditToggle(item.id)}
+              />
+              <Button
+                propsButtonToggle={item.isButtonToggle}
+                propsClassName={"btn btn-warning button-style"}
+                propsOnClick={() => this.props.handleIsDone(item.id)}
+                propsName={"Done"}
+              />
+              <Button
+                propsButtonToggle={item.isButtonToggle}
+                propsClassName={"btn btn-danger button-style"}
+                propsOnClick={() => this.props.handleDeleteByID(item.id)}
+                propsName={"Delete"}
+              />
+
+              {/* <button
+              className="btn btn-warning button-style"
+              onClick={() => props.handleIsDone(item.id)}
+            >
+              Done
+            </button>
+            <button
+              className="btn btn-danger button-style"
+              onClick={() => props.handleDeleteByID(item.id)}
+              //onClick={this.handleDeleteByID(item.id)}
+            >
+              Delete
+            </button> */}
+              <br />
+            </React.Fragment>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+Child.propTypes = {
+  wishList: PropTypes.array.isRequired,
+  handleDeleteByID: PropTypes.func.isRequired,
+  handleIsDone: PropTypes.func.isRequired,
+};
+
+export default Child;
---
+++ src/wishListApp/Parent.js
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+import { v4 as itemID } from "uuid";
+
+import Child from "./Child";
+import Child2 from "./Child2";
+
+import "./Parent.css";
+
+class Parent extends Component {
+  state = {
+    wishList: [
+      {
+        id: itemID(),
+        wish: "To be famous",
+        isDone: false,
+        isEditToggle: false,
+        isButtonToggle: false,
+      },
+    ],
+    inputWish: "",
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    let newWishArray = [
+      ...this.state.wishList,
+      {
+        id: itemID(),
+        wish: this.state.inputWish,
+        isDone: false,
+        isEditToggle: false,
+      },
+    ];
+
+    this.setState({
+      wishList: newWishArray,
+      inputWish: "",
+    });
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      inputWish: event.target.value,
+    });
+  };
+
+  handleDeleteByID = (id) => {
+    // let filteredWishListArray = this.state.wishList.filter(
+    //   (item) => item.id !== id
+    // );
+
+    let filteredWishListArray = this.state.wishList.filter((item) => {
+      if (item.id !== id) {
+        item.isButtonToggle = false;
+        return item;
+      }
+    });
+
+    this.setState({
+      wishList: filteredWishListArray,
+    });
+  };
+
+  handleIsDone = (id) => {
+    //go through the array and find the ID of the wish
+    //Then flip the wish from false to true or true to false
+    //then set new state
+
+    let updatedWishArray = this.state.wishList.map((item) => {
+      if (item.id === id) {
+        item.isDone = !item.isDone;
+      }
+      return item;
+    });
+    this.setState({
+      wishList: updatedWishArray,
+    });
+  };
+
+  handleEditToggle = (id) => {
+    let toggleWishList = this.state.wishList.map((item) => {
+      if (item.id === id) {
+        item.isEditToggle = !item.isEditToggle;
+      }
+
+      // else {
+      //   item.isButtonToggle = !item.isButtonToggle;
+      // }
+
+      if (item.id !== id) {
+        item.isButtonToggle = !item.isButtonToggle;
+      }
+
+      return item;
+    });
+
+    this.setState({
+      wishList: toggleWishList,
+    });
+  };
+
+  updateWish = (id, newTodoItem) => {
+    let updatedWish = this.state.wishList.map((item) => {
+      if (item.id === id) {
+        item.wish = newTodoItem;
+      }
+      return item;
+    });
+
+    this.setState({
+      wishList: updatedWish,
+    });
+  };
+
+  render() {
+    return (
+      <div className="parent-container">
+        <Child2
+          //onSubmit={this.handleSubmit}
+          handleSubmit={this.handleSubmit}
+          handleOnChange={this.handleOnChange}
+          inputWish={this.state.inputWish}
+        />
+        <Child
+          wishList={this.state.wishList}
+          handleDeleteByID={this.handleDeleteByID}
+          handleIsDone={this.handleIsDone}
+          handleEditToggle={this.handleEditToggle}
+          updateWish={this.updateWish}
+        />
+      </div>
+    );
+  }
+}
+
+export default Parent;
